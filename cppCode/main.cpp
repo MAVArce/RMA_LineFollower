97c4c6c744c0f84a6a6295ab72c4e22fb244d69e
MAVArce <marcosarce@usp.br>
2022-07-15 21:29:41 -0300

    Add argv for diferent servers

--- cppCode/main.cpp
+++ cppCode/main.cpp
@@ -20,7 +20,7 @@
 // simExtRemoteApiStart(portNumber) -- inicia servidor remoteAPI do V-REP
 
 extern "C" {
-  #include "remoteApi/extApi.h"
+    #include "remoteApi/extApi.h"
 }
 
 #include <iostream>
@@ -28,61 +28,63 @@ extern "C" {
 
 using namespace std;
 
-int main(int argc, char **argv) 
-{
-  string serverIP = "127.0.0.1";
-  int serverPort = 19999;
-  int leftMotorHandle = 0;
-  float vLeft = 0;
-  int rightMotorHandle = 0;
-  float vRight = 0;
-  string sensorNome[16];
-  int sensorHandle[16];
-  
-  // variaveis de cena e movimentação do pioneer
-  float noDetectionDist=0.5;
-  float maxDetectionDist=0.2;
-  float detect[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-  float braitenbergL[16]={-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-  float braitenbergR[16]={-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-  float v0=2;
-  
-  int clientID=simxStart((simxChar*)serverIP.c_str(),serverPort,true,true,2000,5);
-  
-  if (clientID!=-1)
-  {
-    cout << "Servidor conectado!" << std::endl;
-    
-    // inicialização dos motores
-    if(simxGetObjectHandle(clientID,(const simxChar*) "/PioneerP3DX/leftMotor",(simxInt *) &leftMotorHandle, (simxInt) simx_opmode_oneshot_wait) != simx_return_ok)
-      cout << "Handle do motor esquerdo nao encontrado!" << std::endl;  
-    else
-      cout << "Conectado ao motor esquerdo!" << std::endl;
-    
-    if(simxGetObjectHandle(clientID,(const simxChar*) "/PioneerP3DX/rightMotor",(simxInt *) &rightMotorHandle, (simxInt) simx_opmode_oneshot_wait) != simx_return_ok)
-      cout << "Handle do motor direito nao encontrado!" << std::endl;  
-    else
-      cout << "Conectado ao motor direito!" << std::endl;
-    
-    
-    // desvio e velocidade do robô
-    while(simxGetConnectionId(clientID)!=-1) // enquanto a simulação estiver ativa
-    {
-      vLeft = 0.5;
-      vRight = 0.5;
-      
-      // atualiza velocidades dos motores
-      simxSetJointTargetVelocity(clientID, leftMotorHandle, (simxFloat) vLeft, simx_opmode_streaming);
-      simxSetJointTargetVelocity(clientID, rightMotorHandle, (simxFloat) vRight, simx_opmode_streaming);
-      
-      // espera um pouco antes de reiniciar a leitura dos sensores
-      extApi_sleepMs(5);
+int main(int argc, char **argv) {
+    string serverIP;
+
+    if (argc > 1) {
+        char* ip = argv[1];
+        serverIP = string(ip);
+    } else {
+        serverIP = "172.0.0.1";
     }
-      
-    simxFinish(clientID); // fechando conexao com o servidor
-    cout << "Conexao fechada!" << std::endl;
-  }
-  else
-    cout << "Problemas para conectar o servidor!" << std::endl;
-  return 0;
+
+    int serverPort = 19999;
+    int leftMotorHandle = 0;
+    float vLeft = 0;
+    int rightMotorHandle = 0;
+    float vRight = 0;
+    string sensorNome[16];
+    int sensorHandle[16];
+
+    // variaveis de cena e movimentação do pioneer
+    float v0 = 2;
+
+    int clientID = simxStart((simxChar *)serverIP.c_str(), serverPort, true, true, 2000, 5);
+
+    if (clientID != -1) {
+        cout << "Servidor conectado!" << std::endl;
+
+        // inicialização dos motores
+        if (simxGetObjectHandle(clientID, (const simxChar *)"/PioneerP3DX/leftMotor", (simxInt *)&leftMotorHandle, (simxInt)simx_opmode_oneshot_wait) != simx_return_ok){
+            cout << "Handle do motor esquerdo nao encontrado!" << std::endl;
+        } else {
+            cout << "Conectado ao motor esquerdo!" << std::endl;
+        }
+
+        if (simxGetObjectHandle(clientID, (const simxChar *)"/PioneerP3DX/rightMotor", (simxInt *)&rightMotorHandle, (simxInt)simx_opmode_oneshot_wait) != simx_return_ok) {
+            cout << "Handle do motor direito nao encontrado!" << std::endl;
+        } else {
+            cout << "Conectado ao motor direito!" << std::endl;
+        }
+
+        // desvio e velocidade do robô
+        while (simxGetConnectionId(clientID) != -1) {// enquanto a simulação estiver ativa 
+            vLeft = 0.5;
+            vRight = 0.5;
+
+            // atualiza velocidades dos motores
+            simxSetJointTargetVelocity(clientID, leftMotorHandle, (simxFloat)vLeft, simx_opmode_streaming);
+            simxSetJointTargetVelocity(clientID, rightMotorHandle, (simxFloat)vRight, simx_opmode_streaming);
+
+            // espera um pouco antes de reiniciar a leitura dos sensores
+            extApi_sleepMs(5);
+        }
+
+        simxFinish(clientID); // fechando conexao com o servidor
+        cout << "Conexao fechada!" << std::endl;
+    } else{
+        cout << "Problemas para conectar o servidor!" << std::endl;
+    }
+
+    return 0;
 }
